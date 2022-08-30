977137db3e36e7aa65dc1c41dce901a92518f6bc
Rodrigo Belchior <rodrigo.belchior@conecthus.org.br>
2022-08-30 15:46:31 -0400

    Finalizado desafio de refatoração de classes e typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon?: string;
+  placeholder?: string;
+}
+
+export const Input = ({ name, placeholder, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -37,11 +43,10 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
-
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
+        placeholder={placeholder}
         defaultValue={defaultValue}
         ref={inputRef}
         {...rest}
@@ -50,4 +55,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   );
 };
 
-export default Input;
